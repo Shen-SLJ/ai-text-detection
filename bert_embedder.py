d3f5650f4612a38f7e47a7c4330c12a675bc278e
Shen-SLJ <shen.jng1@gmail.com>
2025-05-08 23:23:06 +1000

    now able to train a linear svm based on input data

--- bert_embedder.py
+++ bert_embedder.py
@@ -1,13 +1,16 @@
 import numpy
 import torch
+import numpy as np
 from transformers import BertTokenizer, BertModel
-
+from sklearn.svm import LinearSVC
 
 # Encode some text
 text = [
-    "Test sentence for BERT embedding, and this is an amazing second sentence. Highly recommend it.",
-    "Second data point test",
+    "Awesome!, this is amazing.",
+    "I should really stop trying to do this.",
 ]
+
+labels = np.array([1, 0])
 
 
 def get_bert_embedding(text: list[str]) -> numpy.ndarray:
@@ -29,6 +32,15 @@ def get_bert_embedding(text: list[str]) -> numpy.ndarray:
             token_type_ids=input["token_type_ids"],
             attention_mask=input["attention_mask"],
         )  # BertModel.forward(), https://huggingface.co/docs/transformers/en/model_doc/bert#transformers.BertModel.forward
-        embeddings = outputs.last_hidden_state[:, 0, :].numpy()  # Use [CLS] token embedding, which contains context of entire sentence
+        embeddings = outputs.last_hidden_state[
+            :, 0, :
+        ].numpy()  # Use [CLS] token embedding, which contains context of entire sentence
 
         return embeddings
+
+
+svm = LinearSVC()
+svm.fit(X=get_bert_embedding(text), y=labels)
+
+test_text = ["good", "bad"]
+print(svm.predict(get_bert_embedding(test_text)))
