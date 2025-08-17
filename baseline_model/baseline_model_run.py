348668f74c81e1fef130d28b9d013ed4ba307d16
Shen-SLJ <shen.jng1@gmail.com>
2025-08-18 02:00:54 +1000

    refactored baseline model into class

--- baseline_model/baseline_model_run.py
+++ baseline_model/baseline_model_run.py
@@ -1,21 +1,15 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.svm import LinearSVC
 from utils.pickle_utils import load_from_pickle
+from baseline_model.baseline_model_train import BaselineModel
 
 TEST_DOCUMENTS = [
   "This is a sample text.",
   "Here is another example of text. Text processing is fun!"
 ]
 
-VECTORIZER_FILENAME_TO_LOAD = "baseline_vectorizer.pkl"
-MODEL_FILENAME_TO_LOAD = "baseline_model.pkl"
+BASELINE_MODEL_FILENAME = "baseline_model.pkl"
 
 # Prediction
-vectorizer = load_from_pickle(VECTORIZER_FILENAME_TO_LOAD, CountVectorizer)
-linear_svm = load_from_pickle(MODEL_FILENAME_TO_LOAD, LinearSVC)
-
-X_test = vectorizer.transform(TEST_DOCUMENTS)
-
-prediction = linear_svm.predict(X_test)
+baseline_model = load_from_pickle(BASELINE_MODEL_FILENAME, BaselineModel)
+prediction = baseline_model.predict(TEST_DOCUMENTS)
 
 print("Prediction:", prediction)
--- baseline_model/baseline_model_train.py
+++ baseline_model/baseline_model_train.py
@@ -1,29 +1,48 @@
+from typing import List
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import LinearSVC
 from utils.pickle_utils import save_to_pickle
 
 TRAIN_DOCUMENTS = [
-  "This is a sample text.", 
-  "Here is another example of text. Text processing is fun!",
+    "This is a sample text.",
+    "Here is another example of text. Text processing is fun!",
 ]
-TRAIN_LABELS = [0, 1] 
+TRAIN_LABELS = [0, 1]
 
-SAVED_VECTORIZER_FILENAME = "baseline_vectorizer.pkl"
-SAVED_MODEL_FILENAME = "baseline_model.pkl"
+class BaselineModel:
+    """A baseline model for text classification using CountVectorizer and LinearSVC.
+    
+    Model architecture based on - Detection of AI-generated Text: An Experimental Study [2024]
+    https://ieeexplore.ieee.org/document/10731116
+    """
+    SAVED_BASELINE_MODEL_FILENAME = "baseline_model.pkl"
 
-# Pre-processing: Vectorization 
-vectorizer = CountVectorizer()
+    def __init__(self, train_documents: list[str], train_labels: list[int]):
+        self.train_documents = train_documents
+        self.train_labels = train_labels
 
-vectorizer.fit(TRAIN_DOCUMENTS)
+        self.vectorizer = CountVectorizer()
+        self.linear_svm = LinearSVC()
+      
+    def train(self) -> "BaselineModel":
+        self.vectorizer.fit(self.train_documents)
+        
+        X_train = self.vectorizer.transform(self.train_documents)
 
-save_to_pickle(vectorizer, SAVED_VECTORIZER_FILENAME)
+        self.linear_svm.fit(X_train, self.train_labels)
 
-X_train = vectorizer.transform(TRAIN_DOCUMENTS)
+        return self
 
-# SVM Model
-linear_svm = LinearSVC()
+    def save(self):
+        print(f"Baseline model saved to {self.SAVED_BASELINE_MODEL_FILENAME}")
 
-linear_svm.fit(X_train, TRAIN_LABELS)
+        return save_to_pickle(self, self.SAVED_BASELINE_MODEL_FILENAME)
 
-save_to_pickle(linear_svm, SAVED_MODEL_FILENAME)
+    def predict(self, documents: List[str]):
+        X = self.vectorizer.transform(documents)
 
+        prediction = self.linear_svm.predict(X)
+
+        return prediction
+
+BaselineModel(TRAIN_DOCUMENTS, TRAIN_LABELS).train().save()
