49464c57eeeff3ff634da17b7657db790dacd703
Shen-SLJ <shen.jng1@gmail.com>
2025-08-19 02:22:49 +1000

    Create candidate model RoBERTa train and predict methods

--- candidate_model/model.py
+++ candidate_model/model.py
@@ -2,39 +2,77 @@ from typing import List
 from utils.pickle_utils import save_to_pickle
 from utils.path_utils import abs_path_from_project_path
 from numpy import ndarray
-from transformers import RobertaForSequenceClassification, TrainingArguments, Trainer
+from transformers import (
+    RobertaForSequenceClassification,
+    RobertaTokenizer,
+    TrainingArguments,
+    Trainer,
+)
+from datasets import Dataset
+from torch import argmax
+from torch.nn.functional import softmax
+
 
 class CandidateModel:
     """Candidate model for text classification using RoBERTa"""
 
-    SAVED_MODEL_FILENAME = "candidate_model.pkl"
+    MODEL_SAVE_PATH = abs_path_from_project_path("saved/candidate_model")
 
-    def __init__(self, train_documents: List[str], train_labels: List[int]):
+    def __init__(
+        self,
+        train_documents: List[str],
+        train_labels: List[int],
+        load_from_saved: bool = False,
+    ):
         self.train_documents = train_documents
         self.train_labels = train_labels
 
-        self.model = RobertaForSequenceClassification.from_pretrained('roberta-base')
+        self.tokenizer: RobertaTokenizer = RobertaTokenizer.from_pretrained(
+            "roberta-base"
+        )
+        self.model = RobertaForSequenceClassification.from_pretrained(
+            self.MODEL_SAVE_PATH if load_from_saved else "roberta-base"
+        )
 
     def train(self) -> "CandidateModel":
+        """
+        Train model and save checkpoint.
+        """
         training_args = TrainingArguments(
-            output_dir=abs_path_from_project_path("saved/candidate_model"),
+            output_dir=self.MODEL_SAVE_PATH,
         )
+
+        tokenized_train_docs = self.tokenizer(
+            self.train_documents, padding=True, truncation=True, return_tensors="pt"
+        )
+
+        dataset_dict = {
+            "input_ids": tokenized_train_docs["input_ids"],
+            "attention_mask": tokenized_train_docs["attention_mask"],
+            "labels": self.train_labels,
+        }
+
+        train_dataset = Dataset.from_dict(dataset_dict)
+
         trainer = Trainer(
             model=self.model,
             args=training_args,
-            train_dataset=self.train_documents,
-            eval_dataset=self.train_labels
+            train_dataset=train_dataset,
+            processing_class=self.tokenizer,
         )
-        
-        return self
 
-    def save(self) -> "CandidateModel":
-        save_to_pickle(self, self.SAVED_MODEL_FILENAME)
-
-        print(f"Candidate model saved to {self.SAVED_MODEL_FILENAME}")
+        trainer.train()
 
         return self
 
-    def predict(self, documents: List[str]) -> ndarray:
+    def predict(self, documents: List[str]) -> List[int]:
+        X = self.tokenizer(
+            documents, padding=True, truncation=True, return_tensors="pt"
+        )
 
-        return []
+        output = self.model(**X)
+
+        predictions = softmax(output.logits, dim=-1)
+        predictions_list = argmax(predictions).tolist()
+
+        return predictions_list
--- utils/sklearn_utils.py
+++ utils/sklearn_utils.py
@@ -2,13 +2,15 @@ from sklearn.metrics import confusion_matrix
 from numpy import ndarray
 
 
-def get_confusion_matrix_as_tuple(true_labels: ndarray, pred_labels: ndarray) -> tuple[int, int, int, int]:
-  """
-  Returns the confusion matrix as a tuple (TN, FP, FN, TP).
+def get_confusion_matrix_as_tuple(
+    true_labels: ndarray | list, pred_labels: ndarray | list
+) -> tuple[int, int, int, int]:
+    """
+    Returns the confusion matrix as a tuple (TN, FP, FN, TP).
 
-  Returns:
-    tuple: A tuple containing (TN, FP, FN, TP).
-  """
-  tn, fp, fn, tp = confusion_matrix(true_labels, pred_labels).ravel().tolist()
+    Returns:
+      tuple: A tuple containing (TN, FP, FN, TP).
+    """
+    tn, fp, fn, tp = confusion_matrix(true_labels, pred_labels).ravel().tolist()
 
-  return tn, fp, fn, tp
+    return tn, fp, fn, tp
