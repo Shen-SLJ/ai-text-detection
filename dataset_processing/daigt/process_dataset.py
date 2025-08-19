a91ddac6436a7e9816888eb992389f5f65ec5405
Shen-SLJ <shen.jng1@gmail.com>
2025-08-20 01:22:37 +1000

    create balanced dataset for daigt

--- dataset_processing/daigt/process_dataset.py
+++ dataset_processing/daigt/process_dataset.py
@@ -1,9 +1,9 @@
 from pandas import read_csv
 from numpy import ndarray
 from utils.path_utils import abs_path_from_project_path
-from utils.dataset_utils import balance_two_class_dataset_by_dropping
 
 DATASET_PATH = abs_path_from_project_path("dataset_processing/daigt/dataset.csv")
+DATASET_BALANCED_PATH = abs_path_from_project_path("dataset_processing/daigt/dataset-balanced.csv")
 
 
 class DAIGTDataset:
@@ -14,10 +14,7 @@ class DAIGTDataset:
 
         Dataset source: https://www.kaggle.com/datasets/thedrcat/daigt-v2-train-dataset
         """
-        dataset = read_csv(DATASET_PATH)
-
-        if balanced:
-            dataset = balance_two_class_dataset_by_dropping(dataset, 'label')
+        dataset = read_csv(DATASET_BALANCED_PATH if balanced else DATASET_PATH)
 
         X = dataset["text"]
         y = dataset["label"]
--- utils/dataset_utils.py
+++ utils/dataset_utils.py
@@ -1,7 +1,7 @@
 from pandas import DataFrame
 
 
-def balance_two_class_dataset_by_dropping(dataset: DataFrame, label_identifier: str) -> DataFrame:
+def correct_imbalance_by_dropping_and_save(dataset: DataFrame, label_identifier: str, save_path: str) -> None:
     label_counts = dataset[label_identifier].value_counts()
     minority_label_count = label_counts[label_counts.idxmin()]
     majority_label = label_counts.idxmax()
@@ -11,4 +11,6 @@ def balance_two_class_dataset_by_dropping(dataset: DataFrame, label_identifier: 
     num_rows_to_drop = len(dataset_majority) - minority_label_count
     rows_to_drop = dataset_majority.sample(num_rows_to_drop).index
 
-    return dataset.drop(rows_to_drop)
+    balanced_dataset = dataset.drop(rows_to_drop)
+
+    balanced_dataset.to_csv(save_path)
