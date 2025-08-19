bf0e5a608457a0e08e37916b6f5c5397c6450f85
Shen-SLJ <shen.jng1@gmail.com>
2025-08-19 20:32:32 +1000

    create dataset concatenation script, add new training (okemdad) dataset

--- dataset_processing/datasets.py
+++ dataset_processing/datasets.py
@@ -1,11 +1,39 @@
+import numpy as np
 from numpy import ndarray
 from dataset_processing.daigt.process_dataset import DAIGTDataset
 from dataset_processing.pratyushi.process_dataset import PratyushiDataset
+from dataset_processing.okemdad_ai.process_dataset import OkemdadDataset
+
+TRAIN_DATASETS = [
+    DAIGTDataset.get(),
+    OkemdadDataset.get(10000)
+]
+EVAL_DATASETS = [
+    PratyushiDataset.get()
+]
 
 def get_train_dataset() -> tuple[ndarray, ndarray]:
     """Load training dataset and return features and labels respectively."""
-    return DAIGTDataset.get()
+    X, y = __concatenate_dataset(TRAIN_DATASETS)
+
+    return X, y
 
 def get_eval_dataset() -> tuple[ndarray, ndarray]:
     """Load evaluation dataset and return features and labels respectively."""
-    return PratyushiDataset.get()
+    X, y = __concatenate_dataset(EVAL_DATASETS)
+
+    return X, y
+
+def __concatenate_dataset(dataset: list[tuple[ndarray, ndarray]]) -> tuple[ndarray, ndarray]:
+    X = np.array([])
+    y = np.array([])
+
+    for dataset in dataset:
+        dataset_X = dataset[0]
+        dataset_y = dataset[1]
+
+        X = np.concatenate((X, dataset_X))
+        y = np.concatenate((y, dataset_y))
+
+    return X, y
+    
---
+++ dataset_processing/okemdad_ai/process_dataset.py
@@ -0,0 +1,23 @@
+from pandas import read_parquet
+from numpy import ndarray
+from typing import Optional
+
+class OkemdadDataset:
+
+  @staticmethod
+  def get(sample_n: Optional[int] = None) -> tuple[ndarray, ndarray]:
+    """Load Okokemdad/ai_text_dataset dataset and return features and labels respectively.
+
+    This dataset consists of 100% AI generated text.
+    
+    Dataset source: https://huggingface.co/datasets/okemdad/ai_text_dataset 
+    """
+    dataset = read_parquet("hf://datasets/okemdad/ai_text_dataset/data/train-00000-of-00001.parquet")
+
+    if sample_n:
+      dataset = dataset.sample(sample_n)
+    
+    X = dataset["text"]
+    y = dataset["label"]
+
+    return X.to_numpy(), y.to_numpy() 
