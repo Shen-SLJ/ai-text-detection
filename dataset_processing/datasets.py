22dc5f1c7a5858db7b0d663aae621b0312a75d42
Shen-SLJ <shen.jng1@gmail.com>
2025-08-21 12:02:21 +1000

    increase dataset size for training roberta

--- dataset_processing/datasets.py
+++ dataset_processing/datasets.py
@@ -5,8 +5,8 @@ from dataset_processing.pratyushi.process_dataset import PratyushiDataset
 
 TRAIN_DATASETS = [
     HumanVsLLMDataset.get_randomly_sampled(
-        human_sample_n=10000,
-        ai_sample_n=10000
+        human_sample_n=20000,
+        ai_sample_n=20000
     )
 ]
 EVAL_DATASETS = [PratyushiDataset.get()]
