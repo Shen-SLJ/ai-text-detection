7f0f3a9295f6d796df74f461b4bba0cbfda2a301
Shen-SLJ <shen.jng1@gmail.com>
2025-08-29 02:09:43 +1000

    change from svm to xgboost

--- candidate_model/stylometric/model.py
+++ candidate_model/stylometric/model.py
@@ -1,19 +1,22 @@
 import numpy as np
-from scipy.sparse import hstack
+from xgboost import XGBClassifier
 from typing import Iterable
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.svm import LinearSVC
 from utils.pickle_utils import save_to_pickle
 from utils.stylometric_utils import (
     get_sentence_burstiness_score,
-    get_flesch_reading_ease_score,
 )
 from utils.matrix_manip_utils import combine_spmatrix_with_1d_nparrays
 from numpy import ndarray
 
 
 class StylometricModel:
-    """A model combining stylometrics with an SVM to detect AI generated text."""
+    """A model combining stylometrics with an SVM to detect AI generated text.
+    
+    Args:
+        classifier: The classifier to use. Should be an sklearn compatible classifier.
+    """
 
     SAVED_MODEL_FILENAME = "candidate_stylometric.pkl"
 
@@ -22,14 +25,15 @@ class StylometricModel:
         self.train_labels = train_labels
 
         self.ngram_vectorizer = HashingVectorizer(ngram_range=(3, 5))
-        self.linear_svm = LinearSVC(max_iter=10000)
+        #self.classifier = LinearSVC(max_iter=10000)
+        self.classifier = XGBClassifier(objective="binary:hinge", device="cpu")
 
     def train(self) -> "StylometricModel":
         self.ngram_vectorizer.fit(self.train_documents)
 
         X_train = self.__get_feature_representation(self.train_documents)
 
-        self.linear_svm.fit(X_train, self.train_labels)
+        self.classifier.fit(X_train, self.train_labels)
 
         return self
 
@@ -54,6 +58,6 @@ class StylometricModel:
     def predict(self, documents: Iterable[str]) -> ndarray:
         X = self.__get_feature_representation(documents)
 
-        prediction = self.linear_svm.predict(X)
+        prediction = self.classifier.predict(X)
 
         return prediction
