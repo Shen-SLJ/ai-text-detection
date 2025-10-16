2956c90bf3bf722cbfdc09648ac5eaad12e98c78
Shen-SLJ <shen.jng1@gmail.com>
2025-10-16 18:54:37 +1100

    add generalisation evaluation script for embedding stylometric

--- candidate_model/embedding_stylometric/model_generalisation_eval.py
+++ candidate_model/embedding_stylometric/model_generalisation_eval.py
@@ -1,14 +1,16 @@
-from candidate_model.roberta.model import RobertaModel
+from candidate_model.embedding_stylometric.model import EmbeddingStylometricModel
 from dataset_processing.datasets import get_eval_dataset
 from utils.metric_utils import print_important_metrics, get_confusion_matrix_as_tuple
+from utils.pickle_utils import load_from_pickle
 
 
 X_eval, y_eval = get_eval_dataset()
-X_eval = X_eval.tolist()
 
-roberta_model = RobertaModel(load_checkpoint_number=200)
+candidate_model = load_from_pickle(
+    EmbeddingStylometricModel.SAVED_MODEL_FILENAME, EmbeddingStylometricModel
+)
 
-prediction = roberta_model.predict(X_eval)
+prediction = candidate_model.predict(X_eval)
 
 tn, fp, fn, tp = get_confusion_matrix_as_tuple(y_eval, prediction)
 
